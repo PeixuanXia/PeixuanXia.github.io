817e3420a1ca208ec4528f4a2058ba7a41dcd87b
xPXXX <xpxxx@XiaPeixuandeMacBook-Pro.local>
2020-04-08 03:45:28 +0800

    新页面

---
+++ _posts/2020-04-08-eclipse-jdt-abstract-syntax-tree-and-the-java-model-beginner.md
@@ -0,0 +1,7 @@
+---
+title: Eclipse JDT - Abstract Syntax Tree (AST) and the Java Model入门
+tags: JDT
+key: akeyname
+---
+
+这是一个测试页面
---
+++ _site/assets/search.js
@@ -0,0 +1,1 @@
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Eclipse JDT - Abstract Syntax Tree (AST) and the Java Model入门",'url':"/2020/04/08/eclipse-jdt-abstract-syntax-tree-and-the-java-model-beginner.html"}]};
